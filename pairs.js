afce7c461d06d5fdbf3b49d5c56240f69eee7876
FrostSource <jjhfrost@gmail.com>
2022-06-09 11:57:53 +1200

    Add matching functionality.
    Boxes flip when clicked and will flip back over if the two don't match.

--- pairs.js
+++ pairs.js
@@ -18,16 +18,79 @@ Pairs.Symbols =
     "local_florist"
 ];
 
+/**
+ * @type {PairBox}
+ */
+Pairs.PreviousFlipped = null;
+
+Pairs.TotalPairFlips = 0;
+Pairs.TotalMatchedFlips = 0;
+
+/**
+ * @typedef {HTMLDivElement & {image: String}} PairBox
+ */
+
+/**
+ * 
+ * @param {string} id 
+ * @param {HTMLElement} parent 
+ * @param {string} image 
+ * @returns 
+ */
 Pairs.CreatePairBox = (id, parent, image)=>
 {
-    const box = CreateGameElement(`PairsBox${id}`, GAME_ELEMENT_TYPE.BUTTON_STATIC, parent);
+    const box = (/**@type {PairBox}*/(CreateGameElement(`PairsBox${id}`, null, parent)));
+    box.image = image;
     box.classList.add("PairsBox");
     const boxInner = CreateGameElement("", "PairsBoxInner", box);
-    const boxFront = CreateGameElement("", "PairsBoxFront", boxInner);
+    const boxFront = CreateGameElement("", GAME_ELEMENT_TYPE.BUTTON_STATIC, boxInner);
+    boxFront.classList.add("PairsBoxFront");
     const boxBack = CreateGameElement("", "PairsBoxBack", boxInner);
     const img = CreateElement("span",null,"material-symbols-outlined",boxBack);
     img.textContent = image;
     return box;
+};
+
+/**
+ * 
+ * @param {Object} e 
+ * @param {HTMLElement} e.target
+ */
+Pairs.BoxClickCallback = (e)=>
+{
+    const box = (/**@type {PairBox}*/(e.target.parentElement.parentElement));
+    DisableGameElement(box);
+    box.classList.add("flipped");
+    if (Pairs.PreviousFlipped)
+    {
+        console.log(e.target.classList);
+        const image = box.image;
+        const prevImage = Pairs.PreviousFlipped.image;
+        console.log(`Image: '${image}', '${prevImage}'`);
+        if (image == prevImage)
+        {
+            // Matched flip
+            Pairs.TotalMatchedFlips++;
+            console.log("Matched flip!");
+        }
+        else
+        {
+            const prevFlipped = Pairs.PreviousFlipped;
+            setTimeout(()=>{
+                box.classList.remove("flipped");
+                prevFlipped.classList.remove("flipped");
+                EnableGameElement(box);
+                EnableGameElement(prevFlipped);
+            }, 1000);
+            console.log("Unmatching flip");
+        }
+        Pairs.TotalPairFlips++;
+        Pairs.PreviousFlipped = null;
+    }
+    else
+    {
+        Pairs.PreviousFlipped = box;
+    }
 };
 
 Pairs.Setup = ()=>
@@ -48,6 +111,7 @@ Pairs.Setup = ()=>
         for (let col = 0; col < Pairs.COLUMNS; col++) {
             const id = `${row}-${col}`;
             const b = Pairs.CreatePairBox(id, pairsContainerRow, symbols.pop());
+            AddElementClickCallback(b, Pairs.BoxClickCallback);
         }
     }
 };
