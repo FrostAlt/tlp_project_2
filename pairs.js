3bb35b06ae2e22aa3185ef4b0545612e454004e2
FrostSource <jjhfrost@gmail.com>
2022-06-09 11:08:16 +1200

    Initial showcase of flipping image boxes.

--- pairs.js
+++ pairs.js
@@ -6,16 +6,51 @@ Pairs.ROWS = 4;
 Pairs.COLUMNS = 4;
 Pairs.ElementArea = null;
 
+Pairs.Symbols =
+[
+    "spoke",
+    "circle",
+    "landscape",
+    "filter_vintage",
+    "brightness_7",
+    "savings",
+    "storefront",
+    "local_florist"
+];
+
+Pairs.CreatePairBox = (id, parent, image)=>
+{
+    const box = CreateGameElement(`PairsBox${id}`, null, parent);
+    box.classList.add("PairsBox");
+    const boxInner = CreateGameElement("", "PairsBoxInner", box);
+    const boxFront = CreateGameElement("", "PairsBoxFront", boxInner);
+    const boxBack = CreateGameElement("", "PairsBoxBack", boxInner);
+    const img = CreateElement("span",null,"material-symbols-outlined",boxBack);
+    img.textContent = image;
+    return box;
+};
+
 Pairs.Setup = ()=>
 {
+    // Shuffle original array so we get random images regardless of square count
+    ShuffleArray(Pairs.Symbols);
+    let symbols = Pairs.Symbols.slice(0, Pairs.ROWS*2);
+    // Multiply image count
+    symbols = symbols.concat(symbols);
+    // Shuffle again to randomize appearance
+    for (let i = 0; i < 6; i++) {
+        ShuffleArray(symbols);
+    }
+
     Pairs.ElementArea = CreateGameElement("PairsContainer");
     for (let row = 0; row < Pairs.ROWS; row++) {
         const pairsContainerRow = CreateGameElement("", "PairsContainerRow", Pairs.ElementArea);
         for (let col = 0; col < Pairs.COLUMNS; col++) {
             const id = `${row}-${col}`;
-            const b = CreateGameElement(`PairsBox${id}`, GAME_ELEMENT_TYPE.BUTTON, pairsContainerRow);
-            b.classList.add("PairsBox");
-            b.textContent = id;
+            // const b = CreateGameElement(`PairsBox${id}`, GAME_ELEMENT_TYPE.BUTTON, pairsContainerRow);
+            const b = Pairs.CreatePairBox(id, pairsContainerRow, symbols.pop());
+            // b.classList.add("PairsBox");
+            // b.textContent = id;
         }
     }
 };
